c844103a65882c29ce65fef9b6d1eca71fdfa144
robertshein <robertshein21@gmail.com>
2026-05-29 09:48:40 +0500

    Создание перемещения. Шейн

--- DragAndDrop_Shein/MainWindow.xaml.cs
+++ DragAndDrop_Shein/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DragAndDrop_Shein
 {
@@ -20,9 +21,18 @@ namespace DragAndDrop_Shein
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 0, 0, 1000 / 60);
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e) 
+        {
+            image.Margin = new Thickness(Mouse.GetPosition(this).X - 25, Mouse.GetPosition(this).Y - 25, 0, 0);
         }
 
         private void image_MouseUp(object sender, MouseButtonEventArgs e)
